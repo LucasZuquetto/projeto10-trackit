be4b7e6a27e8c72c6ad3eb3dac93fe1674c9b898
Lucas Zuquetto <lucasdeozuquetto@hotmail.com>
2022-08-03 21:39:06 -0300

    Envio de dados para login para a API

--- src/Components/LoginScreen.js
+++ src/Components/LoginScreen.js
@@ -1,15 +1,53 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../assets/logo.png";
+import { loginUser } from "../services/requests";
 import LoginStyle from "../Styled-components/LoginStyle";
 
 export default function LoginScreen() {
+  const navigate = useNavigate();
+  const [formLogin, setFormLogin] = useState({
+    email: "",
+    password: "",
+  });
+  function handleForm(e) {
+    setFormLogin({
+      ...formLogin,
+      [e.target.name]: e.target.value,
+    });
+    console.log(formLogin);
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    const promise = loginUser(formLogin);
+    console.log(promise);
+    promise.then((e) => {
+      console.log(e); //aqui está o data
+      navigate("/");
+    });
+    promise.catch(() => {
+      alert("houve um erro no seu login");
+    });
+  }
   return (
     <LoginStyle>
       <img src={logo} alt="logo trackit" />
-      <form>
-        <input placeholder="email" type="text" />
-        <input placeholder="senha" type="password" />
-        <button>Entrar</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={formLogin.email}
+          onChange={handleForm}
+          placeholder="email"
+          name="email"
+          type="text"
+        />
+        <input
+          value={formLogin.password}
+          onChange={handleForm}
+          name="password"
+          placeholder="senha"
+          type="password"
+        />
+        <button type="submit">Entrar</button>
       </form>
       <Link to="/cadastro">
         <p>Não tem uma conta? Cadastre-se!</p>
--- src/services/requests.js
+++ src/services/requests.js
@@ -1,12 +1,16 @@
 import axios from "axios";
 
-
 function registerUser(registerObject) {
-  
   return axios.post(
     "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/sign-up",
     registerObject
   );
 }
+function loginUser(loginObject) {
+  return axios.post(
+    "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login",
+    loginObject
+  );
+}
 
-export { registerUser };
+export { registerUser, loginUser };
